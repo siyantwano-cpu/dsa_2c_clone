825aefd0b32ad0c98f0f8111027390a3b17e9749
CL2-PC <CL2-PC@CL2-PC27>
2026-07-07 08:55:39 +0800

    ACT2A_FIRST_PROGRAM_REPOSITORY

---
+++ Pamintuan_Program/src/main/java/com/mycompany/pamintuan_program/Pamintuan_Program.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pamintuan_program;
+import java.util.Scanner;
+/**
+ *
+ * @author CL2-PC
+ */
+public class Pamintuan_Program {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Hello World!");
+        System.out.println("Pamintuan Carl Justine C");
+        System.out.println("Enter A Number");
+        
+        int X = input.nextInt();
+        
+        System.out.println("Enter A Number");
+        
+        int Y = input.nextInt();
+        
+        int sum;
+        
+        sum = X + Y;
+        
+        System.out.println("sum is:" + sum);
+        
+    }
+}
