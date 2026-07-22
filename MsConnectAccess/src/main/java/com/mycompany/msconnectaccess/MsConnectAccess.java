49ab090491ff9873f15b335a9633c0642072f3a7
CL2-PC <CL2-PC@CL2-PC27>
2026-07-22 14:50:40 +0800

    sir mark 7/22/2026

---
+++ MsConnectAccess/src/main/java/com/mycompany/msconnectaccess/MsConnectAccess.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.msconnectaccess;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+
+/**
+ *
+ * @author CL2-PC
+ */
+import java.sql.DriverManager;
+public class MsConnectAccess {
+
+    public static Connection conn() {
+        try {
+            String url = "jdbc:ucanaccess://”C:\\Users\\CL2-PC\\Documents/jaymarkhampaslupa.accdb” ";
+            Connection conn = DriverManager.getConnection(url);
+            return conn;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+
+        
+    }
+}
