9495dc2ed4f687e71cfb6dbbb32d0cff6c2a3e68
CL2-PC <CL2-PC@CL2-PC27>
2026-09-09 14:49:50 +0800

    act2c manlapaz po opo

---
+++ MsConnectAccess/src/main/java/com/mycompany/msconnectaccess/lbl_player2Health.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.msconnectaccess;
+
+/**
+ *
+ * @author CL2-PC
+ */
+class lbl_player2Health {
+
+    static void setText(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
